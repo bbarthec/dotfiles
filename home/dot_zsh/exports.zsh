6673b955957a21a3439e3ecf9847042d8a0809ea
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-05-13 13:49:06 +0200

    Fix PATH and some other typos

--- home/dot_zsh/exports.zsh
+++ home/dot_zsh/exports.zsh
@@ -4,5 +4,5 @@ export VISUAL=vscode
 export WORKSPACE="$HOME/Work"
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-export PATH="$HOME/.bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export PATH="$PATH:$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export HOMEBREW_NO_AUTO_UPDATE=1
